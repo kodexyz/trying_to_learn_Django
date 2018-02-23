2a0c33970ef8628923ffe1e365c0c6f0e0b22c01
AwesomeHaircut <jesusbalbastro@gmail.com>
2018-02-23 04:51:03 -0700

    What was this commit about? I made it yesterday but I don't remember what I wrote about it :-)

--- myproject/polls/views.py
+++ myproject/polls/views.py
@@ -4,5 +4,19 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+
 def index(request):
     return HttpResponse("Hi, my love")
+
+
+def detail(request, question_id):
+    return HttpResponse("This is question: %s." % question_id)
+
+
+def results(request, question_id):
+    response = "You're looking at the result of %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on %s." % question_id)
