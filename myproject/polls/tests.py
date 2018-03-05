b5eb4a655649b1ae9e06553e9058f72b4897295d
AwesomeHaircut <jesusbalbastro@gmail.com>
2018-03-05 07:53:08 -0700

    EVEN MORE TESTS. But only the skeletons i'm hungry.

--- myproject/polls/tests.py
+++ myproject/polls/tests.py
@@ -2,8 +2,20 @@ import datetime
 
 from django.utils import timezone
 from django.test import TestCase
+from django.urls import reverse
 
 from .models import Question
+
+
+def create_question(question_text, days):
+    """
+    Create a question with the `question_text` and published the
+    given number of `days` offset to now (negative for questions published
+    in the past, positive for questions that have yet to be published).
+    :param question_text:
+    :param days:
+    :return:
+    """
 
 
 class RenamedTestClass(TestCase):
@@ -39,5 +51,44 @@ class RenamedTestClass(TestCase):
         recent_question = Question(pub_date=time)
         self.assertIs(recent_question.was_published_recently(), True)
 
+
+class QuestionIndexViewTests(TestCase):
+    def test_no_questions(self):
+        """
+        If no questions exist, an appropriate message is displayed.
+        :return:
+        """
+        pass
+
+    def test_past_question(self):
+        """
+        Questions with a pub_date in the past are displayed on the
+        index page.
+        :return:
+        """
+        pass
+
+    def test_future_question(self):
+        """
+        Questions with a pub_date in the future aren't displayed on
+        the index page.
+        :return:
+        """
+        pass
+
+    def test_future_question_and_past_question(self):
+        """
+        Even if both past and future questions exist, only past questions
+        are displayed.
+        :return:
+        """
+        pass
+
+    def test_two_past_questions(self):
+        """
+        The questions in index page may display multiple questions.
+        :return:
+        """
+        pass
 
 
